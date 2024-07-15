6cb532b44a593fdecf4d0626db93bc3039fdc450
Dave Hurley <djmhur@gmail.com>
2024-07-15 02:00:42 +0100

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -13,12 +13,15 @@ def main():
     req = c_sk.recv(1024).decode()
     startln, *headers = req.split("\r\n")
     method, path, httpv = startln.split()
-    #if path == "/":
-    text = path[6:]
-    tlen = len(text)
-    c_sk.send(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(tlen)+"\r\n\r\n"+text).encode())
-    #else:
-     #   c_sk.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+    if path == "/":
+        msg = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path.startswith("/echo/"):
+        text = path[6:]
+        tlen = len(text)
+        msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(tlen)+"\r\n\r\n"+text
+    else:
+        msg = "HTTP/1.1 404 Not Found\r\n\r\n"
+    c_sk.send(msg.encode())
     
 
 
