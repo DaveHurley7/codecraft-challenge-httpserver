bcc7ad7446a2a8e7098eeafdcd82af561ff05000
Dave Hurley <djmhur@gmail.com>
2024-07-15 01:31:00 +0100

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     c_sk, addr = server_socket.accept() # wait for client
-    c_sk.send("HTTP/1.1 200 OK\r\n\r\n")
+    c_sk.send("HTTP/1.1 200 OK\r\n\r\n".encode())
     
 
 
