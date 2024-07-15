27fe17ede37a6852520ecec8c8e9ea13183bedf7
Dave Hurley <djmhur@gmail.com>
2024-07-15 01:29:39 +0100

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    c_sk = server_socket.accept() # wait for client
+    c_sk.send("HTTP/1.1 200 OK\r\n\r\n")
+    
 
 
 if __name__ == "__main__":
