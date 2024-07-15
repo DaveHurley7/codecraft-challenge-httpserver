b3fa9a21723447ff6986ccd79065516f5b893ce6
Dave Hurley <djmhur@gmail.com>
2024-07-15 02:10:43 +0100

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -12,11 +12,21 @@ def main():
     c_sk, addr = server_socket.accept() # wait for client
     req = c_sk.recv(1024).decode()
     startln, *headers = req.split("\r\n")
+    req_hdrs = {}
+    for hdr in headers:
+        if hdr == "\r\n":
+            continue
+        hdr_ln = hdr.split(":")
+        hdr[hdr_ln[0].strip()] = hdr_ln[1].strip() 
     method, path, httpv = startln.split()
     if path == "/":
         msg = "HTTP/1.1 200 OK\r\n\r\n"
     elif path.startswith("/echo/"):
         text = path[6:]
+        tlen = len(text)
+        msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(tlen)+"\r\n\r\n"+text
+    elif path.startswith("/useragent"):
+        text = req_hdrs["User-Agent"]
         tlen = len(text)
         msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(tlen)+"\r\n\r\n"+text
     else:
